a74965c727d0ca7e8b96624b3701b668bb8a73f1
durbek <d.ataboyev@hometasker.io>
2023-08-09 18:17:51 +0500

    added dispose and scrollController fix

--- lib/src/widgets/prioritize.dart
+++ lib/src/widgets/prioritize.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// wrap your sliver with this widget if you want to [prioritize] it
+/// [Prior] widgets will be inserted from the 0 index in [slivers] paramether of [CustomScrollView]
 class Prior extends StatelessWidget {
   const Prior ({super.key, required this.child});
 
